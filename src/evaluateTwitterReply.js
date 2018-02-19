73feab8529180f2ac3628334ed19045edf9fe98e
appleJax <appleJax@users.noreply.github.com>
2018-02-19 01:42:04 -0600

    Adjust settings for google cloud platform

--- src/evaluateTwitterReply.js
+++ src/evaluateTwitterReply.js
@@ -22,6 +22,7 @@ export function evaluateResponse({
   }
 }) {
   return new Promise(async (resolve, reject) => {
+    console.log('Received a reply!!!');
     const liveQuestions = await tryCatch(DB.getLiveQuestions());
     const foundQuestion = liveQuestions.find(
       questionCard => questionCard.questionId === questionId
