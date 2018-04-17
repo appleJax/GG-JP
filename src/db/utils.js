003fc31988d94885887abbcd9824191994cc6e0b
appleJax <appleJax@users.noreply.github.com>
2018-04-16 19:12:30 -0500

    fix update stats bug

--- src/db/utils.js
+++ src/db/utils.js
@@ -232,7 +232,10 @@ export function calculateNewStats(
     newHigh.timestamp = currentTimestamp;
   }
 
-  if (avgAnswerTime > 0 && avgAnswerTime <= lowestAvgAnswerTime.value) {
+  if (avgAnswerTime > 0 &&
+      lowestAvgAnswerTime.value === 0 ||
+      avgAnswerTime <= lowestAvgAnswerTime.value
+  ) {
     newLow.value = avgAnswerTime;
     newLow.timestamp = currentTimestamp;
   }
